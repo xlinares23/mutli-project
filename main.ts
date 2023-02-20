e93b68a108cb5e7f2f85e68a73a87f29eaa0f389
jorge-ros <120482252+jorge-ros@users.noreply.github.com>
2023-02-19 23:47:57 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,9 +4,12 @@ namespace SpriteKind {
 }
 function multiplayerScores () {
     addition = info.score() + info.player2.score()
+    // returns the value of the sum of both players scores.
     return addition
 }
+// shooting speed is dependant on the users difficulty chioce.
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    // seleciton
     if (difficulty == "a") {
         projectile = sprites.createProjectileFromSprite(laserArray._pickRandom(), theShip, 0, -85)
     } else if (difficulty == "b") {
@@ -14,6 +17,7 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
         pause(500)
     }
 })
+// functions the same as player 1's code but affects player 2
 controller.player2.onButtonEvent(ControllerButton.A, ControllerButtonEvent.Pressed, function () {
     if (difficulty == "a") {
         projectileTwo = sprites.createProjectileFromSprite(laserArray._pickRandom(), shipPLayerTwo, 0, -85)
@@ -24,6 +28,7 @@ controller.player2.onButtonEvent(ControllerButton.A, ControllerButtonEvent.Press
         pause(500)
     }
 })
+// player 2's code for enemy overlap: works exactly as player 1's code except this only affects player 2.
 sprites.onOverlap(SpriteKind.PlayerTwo, SpriteKind.Enemy, function (sprite, otherSprite) {
     sprites.destroy(stranger, effects.spray, 500)
     info.player2.changeLifeBy(-1)
@@ -33,11 +38,14 @@ sprites.onOverlap(SpriteKind.PlayerTwo, SpriteKind.Enemy, function (sprite, othe
         game.gameOver(false)
     }
 })
+// projectile code for player 2
 sprites.onOverlap(SpriteKind.projectile2, SpriteKind.Enemy, function (sprite, otherSprite) {
     sprites.destroy(stranger, effects.fire, 200)
     info.player2.changeScoreBy(1)
 })
+// based on the multiplayer choice, if the player selected 2 player, this code will create the second player.
 function multiplayerChecker () {
+    // seleciton/condition
     if (multiplayer2 == "b") {
         info.player2.setLife(3)
         shipPLayerTwo = sprites.create(img`
@@ -63,8 +71,11 @@ function multiplayerChecker () {
         shipPLayerTwo.setKind(SpriteKind.PlayerTwo)
     }
 }
+// sequencing
 function mainGame () {
+    // user input
     difficulty = game.askForString("a = easy | b = hard", 1)
+    // user input
     multiplayer2 = game.askForString("a = 1Player | b = 2Player", 1)
     scene.setBackgroundImage(img`
         ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
@@ -210,6 +221,7 @@ function mainGame () {
     controller.moveSprite(theShip, 125, 125)
     theShip.setStayInScreen(true)
     theShip.setKind(SpriteKind.Player)
+    // array
     spriteArray = [
     img`
         . . . . . . . . . . . . . . . . . 
@@ -339,9 +351,13 @@ function mainGame () {
         `
     ]
     multiplayerChecker()
+    // function with parameter
     difficultyChecker(difficulty)
 }
 info.onLifeZero(function () {
+    // if user chooses 1 player, game will end when player reaches life 0.
+    // 
+    // if user chooses 2 player, this ensures the player will simply be out of the game instead of ending the entire game for both players.
     if (multiplayer2 == "a") {
         game.gameOver(false)
     } else if (multiplayer2 == "b") {
@@ -349,13 +365,16 @@ info.onLifeZero(function () {
         theShip.setFlag(SpriteFlag.Ghost, true)
     }
 })
+// based on users difficulty choice, this code chooses how fast and frequently the enemies will spawn
 function difficultyChecker (difficulty: string) {
+    // iteration/conditional statement
     while (difficulty == "a") {
         stranger = sprites.create(spriteArray._pickRandom(), SpriteKind.Enemy)
         stranger.setPosition(randint(0, 155), 0)
         stranger.setVelocity(75, 75)
         pause(1000)
     }
+    // iteration/conditional statement
     while (difficulty == "b") {
         stranger = sprites.create(spriteArray._pickRandom(), SpriteKind.Enemy)
         stranger.setPosition(randint(0, 155), 0)
@@ -363,17 +382,21 @@ function difficultyChecker (difficulty: string) {
         pause(800)
     }
 }
+// functions the same as player 1's code but affects player 2.
 info.player2.onLifeZero(function () {
     sprites.destroy(shipPLayerTwo, effects.spray, 500)
     shipPLayerTwo.setFlag(SpriteFlag.Ghost, true)
 })
+// player 1's projectile code: has to be seperate from player 2 to avoid having two scores when user chooses 1 player mode. If statement wouldn't work because it would add to both scores if any projectile from anyone hit an enemy.
 sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
     sprites.destroy(stranger, effects.fire, 200)
     info.changeScoreBy(1)
 })
+// has to be seperate from player 2's code in order to prevent the same problem mentioned above
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
     sprites.destroy(stranger, effects.spray, 500)
     info.changeLifeBy(-1)
+    // seleciton: makes sure game over happens when both players life is equal to 0 when the user chooses two player mode. Without this, a game over would happen if any player died.
     if (info.life() == 0 && info.player2.life() == 0) {
         multiplayerScores()
         game.showLongText("Total Score: " + addition, DialogLayout.Full)
@@ -390,4 +413,5 @@ let laserArray: Image[] = []
 let projectile: Sprite = null
 let difficulty = ""
 let addition = 0
+// sets up the main game by asking the user their difficulty and whether or not they're playing with two people. It sets up the enemy sprites, laser sprites, the main ship character, and then it calls the other funcitons that makes the game operate.
 mainGame()
